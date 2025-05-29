d54313db9b1e6354078cb2e8e4f41afa06935155
PainInVein <lesybinh2004@gmail.com>
2025-05-29 20:46:47 +0700

    delete uneccessary interface file

--- MV.PresnetationLayer/ServiceInterfaces/IUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using MV.ApplicationLayer.DTO.RequestModel;
-
-namespace MV.PresnetationLayer.ServiceInterfaces
-{
-    public interface ILoginService
-    {
-        Task<bool> LoginUser(LoginRequest loginRequest);
-    }
-}
