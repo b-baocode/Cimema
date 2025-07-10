86f1398a072b391d2e81226e57036712b44acc8d
25_CPL_NET_05_SE183852 <hoangnvse183852@fpt.edu.vn>
2025-07-10 14:31:33 +0700

    Update

--- MV.InfrastructureLayer/Configuration/QuartzConfiguration.cs
+++ MV.InfrastructureLayer/Configuration/QuartzConfiguration.cs
@@ -34,40 +34,40 @@ namespace MV.InfrastructureLayer.Configuration
             });
 
             // Đăng ký job xóa payment cũ
-            services.AddQuartz(q =>
-            {
-                var jobKey = new JobKey("DeleteOldSuccessfulPaymentsJob");
-                q.AddJob<global::MV.InfrastructureLayer.SchedulingRepository.DeleteOldSuccessfulPaymentsJob>(opts => opts.WithIdentity(jobKey));
-                q.AddTrigger(opts => opts
-                    .ForJob(jobKey)
-                    //.WithIdentity("DeleteOldSuccessfulPaymentsJob-trigger")
-                    // .WithSimpleSchedule(x => x
-                    //     .WithIntervalInHours(24)
-                    //     .RepeatForever()
-                    // )
-
-                // Test nên đổi lấy 1 minutes
-                .WithSimpleSchedule(x => x
-                    .WithIntervalInMinutes(1)
-                    .RepeatForever()
-                )
-                );
-            });
+            // services.AddQuartz(q =>
+            // {
+            //     var jobKey = new JobKey("DeleteOldSuccessfulPaymentsJob");
+            //     q.AddJob<global::MV.InfrastructureLayer.SchedulingRepository.DeleteOldSuccessfulPaymentsJob>(opts => opts.WithIdentity(jobKey));
+            //     q.AddTrigger(opts => opts
+            //         .ForJob(jobKey)
+            //         //.WithIdentity("DeleteOldSuccessfulPaymentsJob-trigger")
+            //         // .WithSimpleSchedule(x => x
+            //         //     .WithIntervalInHours(24)
+            //         //     .RepeatForever()
+            //         // )
+            //
+            //     // Test nên đổi lấy 1 minutes
+            //     .WithSimpleSchedule(x => x
+            //         .WithIntervalInMinutes(1)
+            //         .RepeatForever()
+            //     )
+            //     );
+            // });
 
             // Đăng ký job xóa ticket invoice cũ
-            services.AddQuartz(q =>
-            {
-                var jobKey = new JobKey("DeleteOldTicketInvoicesJob");
-                q.AddJob<global::MV.InfrastructureLayer.SchedulingRepository.DeleteOldTicketInvoicesJob>(opts => opts.WithIdentity(jobKey));
-                q.AddTrigger(opts => opts
-                    .ForJob(jobKey)
-                    .WithIdentity("DeleteOldTicketInvoicesJob-trigger")
-                    .WithSimpleSchedule(x => x
-                        .WithIntervalInHours(24)
-                        .RepeatForever()
-                    )
-                );
-            });
+            // services.AddQuartz(q =>
+            // {
+            //     var jobKey = new JobKey("DeleteOldTicketInvoicesJob");
+            //     q.AddJob<global::MV.InfrastructureLayer.SchedulingRepository.DeleteOldTicketInvoicesJob>(opts => opts.WithIdentity(jobKey));
+            //     q.AddTrigger(opts => opts
+            //         .ForJob(jobKey)
+            //         .WithIdentity("DeleteOldTicketInvoicesJob-trigger")
+            //         .WithSimpleSchedule(x => x
+            //             .WithIntervalInHours(24)
+            //             .RepeatForever()
+            //         )
+            //     );
+            // });
 
 
             services.AddQuartzHostedService(options =>
